bbde225abb6d416a500159e16f3360798d395170
AlexandrZl <geedisgood72@gmail.com>
2013-11-03 23:13:53 +0700

    init commit

--- main.rb
+++ main.rb
@@ -1,51 +1,57 @@
 require 'sinatra'
 require 'pony'
-
-
-
+EMAIL_REGEX =  /[\w]+\@[\w]+\.[\w]/
 
 get "/" do
-  @active1='class="active"'
   @title='Main'
   erb :main               
 end
 get "/biography" do
-  @active2='class="active"'
   @title='Bio'
   erb :biography          
 end
 get "/hobby" do
-  @active3='class="active"'
    @title='hobby'
   erb :hobby             
 end
 get "/about" do
-  @active4='class="active"'
    @title='about'
   erb :about              
 end
 get "/contact" do
-  @active5='class="active"'
    @title='contact'
   erb :contact              
 end
+
+before '/contact' do
+  @errors = []
+end
+
 post '/contact' do
-  
+  errors << 'Check entered data email'  unless params[:email].match EMAIL_REGEX
+  @errors << 'Check entered data message' if params[:message].empty?
+  @errors << 'Check entered data name' if params[:name].empty?
+  unless @errors.any?
   options = {
     :to => params[:email],
-    :from => 'put your email',
-    :subject => params[:name],
+    :from => 'puts email',
+    :subject => "Message by #{params[:name]}",
     :html_body => params[:message],
     :via => :smtp,
     :via_options => {
       :address => 'smtp.gmail.com',
       :port => 587,
       :enable_starttls_auto => true,
-      :user_name => 'put your email',
-      :password => 'put your password',
+      :user_name => 'puts email',
+      :password => '[uts password]',
       :authentication => :plain,
       :domain => 'localhost'
     }
   }
   Pony.mail(options)
+  redirect '/hobby'
+  else
+    erb :contact
+  end
+
 end
